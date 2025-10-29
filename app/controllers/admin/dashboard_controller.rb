342c1d25721f259344753b629f007e4ac50ae7b5
Michael Hicks <hicks.michael04@gmail.com>
2025-10-29 10:47:57 -0600

    Create admin dashboard with cards

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,4 +1,9 @@
 class Admin::DashboardController < Admin::BaseController
   def index
+    # Fetch the total count for each model. These will be available in the view.
+    @user_count = User.count
+    @customer_count = Customer.count
+    @machine_count = Machine.count
+    @job_count = Job.count
   end
 end
